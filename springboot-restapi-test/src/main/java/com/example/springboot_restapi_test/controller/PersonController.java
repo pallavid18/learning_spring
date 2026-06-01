f8b331315884adc1fb9cf86debba0b0fc871908b
pallavid18 <pallavidhope9@gmail.com>
2026-06-01 13:29:05 +0530

    Update PersonController.java

--- springboot-restapi-test/src/main/java/com/example/springboot_restapi_test/controller/PersonController.java
+++ springboot-restapi-test/src/main/java/com/example/springboot_restapi_test/controller/PersonController.java
@@ -3,6 +3,7 @@ package com.example.springboot_restapi_test.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/api/persons")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173/")
 public class PersonController {
 
 	@Autowired
